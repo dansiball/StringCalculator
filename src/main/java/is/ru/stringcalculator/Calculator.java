72b409727520eda07219f738b91513b60a0eaf54
Dansiball <manniheitinn@gmail.com>
2017-10-07 21:14:19 +0000

    4th Kata Step Refactored, Function created to get a split string array

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Calculator{
 	public static int add(String text){
 		if (text.contains("-")){
-			String numbers[] = (text.split(",|\\\n"));
+			String numbers[] = getSplit(text);
 			ArrayList<String> list = new ArrayList<String>();
 			for (int i = 0; i < numbers.length; i++){
 				if (numbers[i].contains("-")){
@@ -23,7 +23,7 @@ public class Calculator{
 		}
 		else{
 			if (text.contains(",") || text.contains("\n")){
-				String numbers[] = (text.split(",|\\\n"));
+				String numbers[] = getSplit(text);
 				int total = 0;
 				for (String number : numbers){
 					total+= StringToInt(number);
@@ -36,5 +36,9 @@ public class Calculator{
 
 	private static int StringToInt(String number){
 		return Integer.parseInt(number);
+	}
+
+	private static String[] getSplit(String number){
+		return number.split(",|\\\n");
 	}
 }
