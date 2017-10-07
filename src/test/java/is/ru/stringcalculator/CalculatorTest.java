46c1f6c46c950d55b12a576b5437863b2f91712b
Dansiball <manniheitinn@gmail.com>
2017-10-07 16:16:20 +0000

    1st Kata Step Complete

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -8,4 +8,14 @@ public class CalculatorTest{
 	public void testEmptyString(){
 		assertEquals(0, Calculator.add(""));
 	}
+
+	@Test
+	public void testOneNumber(){
+		assertEquals(5, Calculator.add("5"));
+	}
+
+	@Test
+	public void testTwoNumbers(){
+		assertEquals(8, Calculator.add("3,5"));
+	}
 }
